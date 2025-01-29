abd95cadf6d792944f6d6e88235d404239ee6303
Maria Eduarda <meac21kj@gmail.com>
2025-01-29 07:33:03 -0300

    feat: adicionando a funcionalidade de criar um novo usuário

--- src/main/java/com/meac/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/meac/springsecurity/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/login" ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users" ).permitAll()
                         .anyRequest().authenticated())
                 .csrf(crsf -> crsf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
---
+++ src/main/java/com/meac/springsecurity/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.meac.springsecurity.controller;
+
+import com.meac.springsecurity.controller.dto.CreateUserDTO;
+import com.meac.springsecurity.entities.Role;
+import com.meac.springsecurity.entities.User;
+import com.meac.springsecurity.repositories.RoleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import com.meac.springsecurity.repositories.UserRepository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDTO userDTO) {
+
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+
+        var user =  (userRepository.findByUsername(userDTO.username()));
+        if (user.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        User newUser = new User();
+        newUser.setUsername(userDTO.username());
+        newUser.setPassword(passwordEncoder.encode(userDTO.password()));
+        newUser.setRoles(Set.of(basicRole));
+
+        userRepository.save(newUser);
+
+
+        return ResponseEntity.ok().build();
+    }
+
+}
