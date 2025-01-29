64fa525c64700b8acbfd9d58741844082d200745
Maria Eduarda <meac21kj@gmail.com>
2025-01-29 06:25:00 -0300

    feat: adicionadno rotas de login como exceção na proteção de rotas padrão do Spring Security

--- src/main/java/com/meac/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/meac/springsecurity/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import net.jcip.annotations.Immutable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,7 +38,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/login" ).permitAll()
+                        .anyRequest().authenticated())
                 .csrf(crsf -> crsf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
